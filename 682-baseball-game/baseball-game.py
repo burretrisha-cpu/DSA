8967c69d8368b127d222d3900e28639c210e9f41
burretrisha-cpu <burre.trisha@gmail.com>
2026-08-04 13:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.79%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack=[]
+        for ops in operations:
+            if ops=='C':
+                stack.pop()
+            elif ops=='D':
+                stack.append(2*stack[-1])
+            elif ops=='+':
+                stack.append(stack[-1]+stack[-2])
+            else:
+                stack.append(int(ops))
+        return sum(stack)
+
+
+
+
+        
+        
